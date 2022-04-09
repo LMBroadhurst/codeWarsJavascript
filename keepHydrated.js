aaeae22bc2abc1a3f4ff7490fe751d3d7fd742d8
Lewis Broadhurst <95776633+LMBroadhurst@users.noreply.github.com>
2022-04-09 20:16:39 +0100

    Create keepHydrated.js

---
+++ keepHydrated.js
@@ -0,0 +1,11 @@
+Keep Hydrated!
+
+function litres(time) {
+  let waterTotal = time * 0.5
+  waterTotal = Math.floor(waterTotal)
+  return waterTotal
+}
+
+// Get the number of hours of cycling
+// Calculate how much water is needed for the journey
+// Round value to smallest value
