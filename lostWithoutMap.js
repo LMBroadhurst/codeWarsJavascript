fd4d313ce5afc3c23e144002dc0cf023e129062d
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-04-12 22:07:59 +0100

    Create lostWithoutMap.js

---
+++ lostWithoutMap.js
@@ -0,0 +1,10 @@
+// Beginner - Lost Without a Map
+
+function maps(x){
+  let outputArray = []
+  for (i = 0; i < x.length; i++) {
+    let valueToAdd = x[i] * 2
+    outputArray.push(valueToAdd)
+  }
+  return outputArray
+}
